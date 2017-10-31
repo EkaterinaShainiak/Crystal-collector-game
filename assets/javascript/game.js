5a32e60178b479517b00be5da35d1978069fb14c
Ekaterina Shainiak <ekaterina.shainiak@gmail.com>
2017-10-31 10:05:48 -0700

    added logic;

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,67 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var wins = 0;
     var looses = 0;
     var youScore;
-    var targetScore; 
+    var targetScore;
+    var inGame = false;
 
-    function resetGame(){
+    function resetGame() {
         firstCrystal = getRandomNumber(1, 12);
         secondCrystal = getRandomNumber(1, 12);
         thirdCrystal = getRandomNumber(1, 12);
         foursCrystal = getRandomNumber(1, 12);
-
-
+        targetScore = getRandomNumber(19, 120);
+        inGame = true;
+        youScore = 0;
+        display()
     }
+
     function getRandomNumber(minBound, maxBound) {
-        return Math.floor(Math.random() * 12 + 1);
+        return Math.floor(Math.random() * maxBound + minBound);
     }
     resetGame();
-    console.log()
 
+    function display() {
+        $("#computer-pick").text(targetScore);
+        $("#my-score").text(youScore);
+
+    }
+
+    $("#blue").on("click", function () {
+        youScore += firstCrystal
+        $("#my-score").text(youScore);
+        ifWin()
+        
+        
+    })
+
+    $("#red").on("click", function () {
+        youScore += secondCrystal
+        $("#my-score").text(youScore);
+        ifWin()
+    })
+
+    $("#green").on("click", function () {
+        youScore += thirdCrystal
+        $("#my-score").text(youScore);
+        ifWin()
+    })
+
+    $("#violet").on("click", function () {
+        youScore += foursCrystal
+        $("#my-score").text(youScore);
+        ifWin()
+    })
+
+    function ifWin() {
+        if (youScore > targetScore) {
+            looses++;
+            $("#my-losses").text(looses);
+            resetGame();
+        }
+        if (youScore === targetScore) {
+            wins++;
+            resetGame();
+        }
+    }
 })
